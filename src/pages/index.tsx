cad95ae47ae1e642091c0e49d14951dc0d52924d
deverick <dncellassistec@gmail.com>
2025-05-28 11:38:16 -0300

    inicio do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import styles from "../../styles/home.module.css";
+
+import Image from "next/image";
+
+import heroLogo from "../../public/assets/hero.png"
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+      <title
+       >Tarefas+ | Organize suas tarefas de forma</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.LogoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo tarefas+"
+            src={heroLogo}
+            priority
+          />
+          <h1 className={styles.title}>Sistema feito para você organizar <br />
+          seus estudos e tarefas
+          </h1>
+        </div>
+      </main>
+    </div>
+  );
+}
