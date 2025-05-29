eaddd766c816de3c660456107edf72b1bafe6fbc
deverick <dncellassistec@gmail.com>
2025-05-28 22:46:46 -0300

    criado componentre header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import styles from "../../../styles/header.module.css";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+                tarefas<span>+</span> 
+            </h1>
+          </Link>
+          <Link href="/deshboard" className={styles.link}>
+            Meu Painel 
+          </Link>
+        </nav>
+
+        <button className={styles.loginButton}>Acessar</button>
+      </section>
+    </header>
+  );
+}
