9370ab954b5539eaf248735419310d38cbc61dfe
deverick <dncellassistec@gmail.com>
2025-05-28 22:15:04 -0300

    termino do styles da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,19 @@ export default function Home() {
           <h1 className={styles.title}>Sistema feito para você organizar <br />
           seus estudos e tarefas
           </h1>
+
+          <div className={styles.infoContent}>
+
+            <section className={styles.box}>
+              <span>+12 posts</span>
+            </section>
+
+            <section className={styles.box}>
+              <span>+90 comentarios</span>
+            </section>
+
+          </div>
+
         </div>
       </main>
     </div>
