d60f23f21b9a909f399a85560c760e06b4209b6f
deverick <dncellassistec@gmail.com>
2025-06-05 15:46:35 -0300

    bloqueio da pagina dashboard quando não tiver user logado

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,35 @@
-import styles from './styles.module.css'
-import Head from 'next/head'
+import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
+import Head from "next/head";
+
+import { getSession } from "next-auth/react";
 
 export default function Dashboard() {
-    return(
-        <div className='container'>
-            <Head>
-                <title>Meu painel de tarefas</title>
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
 
-            <h1>Pagina painel</h1>
-        </div>
-    )
+      <h1>Pagina painel</h1>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  //console.log(session)
+
+  if (!session?.user) {
+    return {
+      redirect: { 
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
